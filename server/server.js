dfa6287b709b9842fa5cc751d9705458ffc328ca
Timothy Ong <tim.ong.118@gmail.com>
2014-09-20 16:19:56 -0400

    server side:

---
+++ server/server.js
@@ -0,0 +1,21 @@
+var WebSocket = require('ws');
+var ws = new WebSocket('ws://localhost:7204/myo/1');
+
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var morgan = require('morgan');
+
+server.listen(3000);
+
+app.use(express.static(__dirname + "/public"));
+
+io.sockets.on('connection', function(socket){
+	socket.on('pointable', function(leap){
+		console.log(leap);
+	});
+	ws.on('message', function(myo){
+		console.log(myo);
+	});
+});
